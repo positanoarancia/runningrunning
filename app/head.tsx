03dda9bd5b65cdc2af05cffbcbcd75d637c86886
positanoarancia <33715582+positanoarancia@users.noreply.github.com>
2026-04-15 12:29:21 +0900

    Add explicit Google Search Console verification meta tag

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="f8VLh3Llc8IXBYWkldrHvplHkYIt17No7Sjnuc9TexI"
+      />
+    </>
+  );
+}
